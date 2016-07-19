be59831752561ef36dd73aa171fa6b238b4c68cf
Kobe Neuckermans <kobeneuckermans@gmail.com>
2016-07-19 14:36:21 +0200

    enable deployment on heroku

--- server/main.js
+++ server/main.js
@@ -4,9 +4,29 @@
 
 var express = require('express');
 var app = express();
+var port = 5000;
 
-app.use('/',express.static('app'));
+app.set('port',(process.env.PORT || port));
 
-app.listen(3000,function () {
-	console.log('express is running');
+// setting up the middleware
+app.use(express.static(__dirname + 'public'));
+app.use(express.static(__dirname + 'views'));
+
+// views is directory for all the template files
+app.set('views', __dirname + '/views');
+app.set('view engine','ejs');
+
+app.get('/',function (req,res) {
+	res.send('hello bahrain');
 });
+
+app.get('/books',function (req,res) {
+	res.send('hello books');
+});
+
+app.listen(app.get('port'),function (err) {
+	console.log('Pilotnote app is running on port', app.get('port'));
+});
+
+//TODO: check express calls for validity
+
